4e5c20d5c249d633dc08a43701a7f42634a62683
yagovenci <yago_venci@yahoo.com.br>
2023-01-22 11:52:39 -0300

    first commit

---
+++ main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-06878d265978313ca"
+  instance_type = "t2.micro"
+  key_name = "iac_alura"
+
+  tags = {
+    Name = "Primeira_instacia"
+  }
+}
+
