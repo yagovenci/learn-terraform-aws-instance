609c1a904e9301513ef0e9546615c414fd5f7cee
yagovenci <yago_venci@yahoo.com.br>
2023-01-24 17:23:59 -0300

    adicionando novas tarefafas no terraform

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ terraform {
 }
 
 provider "aws" {
-  profile = "default"
   region  = "us-east-1"
 }
 
@@ -18,9 +17,14 @@ resource "aws_instance" "app_server" {
   ami           = "ami-06878d265978313ca"
   instance_type = "t2.micro"
   key_name = "iac_alura"
-
+  user_data = <<-EOF
+                #!/bin/bash
+                cd /home/ubuntu
+                echo “<h1>Mensagem a ser mostrada</h1>” > index.js
+                nohup busybox httpd -f -p 8080 &
+                EOF
   tags = {
-    Name = "Primeira_instacia"
+    Name = "Novoteste aws"
   }
 }
 
